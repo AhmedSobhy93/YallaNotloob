6d0ae901d02945610a215a6c8410d38bf8fe9856
Zahra Meki <zahrameki18@gmail.com>
2016-04-30 15:11:00 +0200

    login FB

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 	before_action :authenticate_user!
 	def home 
+		#@user = Users.find(params[:id])
+    	@orders = Order.all
+    	#where(user_id: params[:id])
 	end	
 end
