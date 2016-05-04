ec825fafaa43a873fe9739814cb387af6c702315
DahliaFouad <dahlia_m_fouad@hotmail.com>
2016-05-04 23:16:52 +0300

    Update routes.rb
    Add Friends

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,10 @@ Rails.application.routes.draw do
   resources :friends
   resources :group_members
   resources :groups
+  resources :friendships
+
   # devise_for :users, :controllers => { registrations: 'registrations' }
-  devise_for :users , :controllers => {registrations: 'registrations', :omniauth_callbacks => "omniauth_callbacks" }
+  devise_for :users , :controllers => {registrations: 'registrations', :omniauth_callbacks => "users/omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -15,6 +17,8 @@ Rails.application.routes.draw do
   root 'users#home'
 
   get 'users/home' => 'users#home'
+  get 'users/show' => 'users#show'
+  get 'users/index' => 'users#index'
 
   get 'orders/:id/cancel' => 'orders#cancel'
   get 'orders/:id/finish' => 'orders#finish'
